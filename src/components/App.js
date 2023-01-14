cefb0f01551da46e04ab62f63d1d87340831231e
eve-atieno <atienoevelyne53@gmail.com>
2023-01-14 13:17:23 +0300

    add data into App.js file

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
 // create your App component here
+import React, { useState, useEffect } from 'react';
+
+const App = () => {
+  const [dogImage, setDogImage] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then(res => res.json())
+      .then(data => {
+        setDogImage(data.message);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  return <img src={dogImage} alt="A Random Dog" />;
+};
+
+export default App;
